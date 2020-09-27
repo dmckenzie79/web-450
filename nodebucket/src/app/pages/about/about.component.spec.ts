cfc242b5d22e378df10a5b49217e6c1b54a229ae
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-09-27 10:47:35 -0400

    Create about.component.spec.ts

---
+++ nodebucket/src/app/pages/about/about.component.spec.ts
@@ -0,0 +1,32 @@
+/**
+ * Title: about.component.spec.ts
+ * Author: Diandra McKenzie
+ * Date: 23 September 2020
+ * Description: About component spec file
+ */
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AboutComponent } from './about.component';
+
+describe('AboutComponent', () => {
+  let component: AboutComponent;
+  let fixture: ComponentFixture<AboutComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AboutComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AboutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
