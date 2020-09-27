59af6cd82e80d6a34cfa24b9d76f77cc6f6f439c
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-09-27 10:47:39 -0400

    Create about.component.ts

---
+++ nodebucket/src/app/pages/about/about.component.ts
@@ -0,0 +1,22 @@
+/**
+ * Title: about.component.ts
+ * Author: Diandra McKenzie
+ * Date: 23 September 2020
+ * Description: About component file
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
