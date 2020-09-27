97c3d7b2b79d94136936f4056f8f94cd4ccd748e
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-09-27 10:45:22 -0400

    Update app-routing.module.ts

--- nodebucket/src/app/app-routing.module.ts
+++ nodebucket/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+/**
+ * Title: app-routing.module.ts
+ * Author: Professor Krasso
+ * Date: 19 September 2020
+ * Modified By: Diandra McKenzie
+ * Description: Routing Module App file
+ */
+
 import { HomeComponent } from './pages/home/home.component';
 import { BaseLayoutComponent } from './shared/base-layout/base-layout.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { AuthGuard } from './shared/guard/auth.guard';
+import { AuthLayoutComponent } from './shared/auth-layout/auth-layout.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 
 const routes: Routes = [
   {
@@ -10,10 +22,25 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        component: HomeComponent
-      }
+        component: HomeComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      },
     ]
-  }
+  },
+      {
+        path: 'session',
+        component: AuthLayoutComponent,
+        children: [
+          {
+            path: 'sign-in',
+            component: SignInComponent
+          }
+        ]
+      },
 ];
 
 @NgModule({
