b0a6e9d8ef0d7ca6fd7f68becd32eae6e585a467
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-09-27 10:44:55 -0400

    Update app.po.ts

--- nodebucket/e2e/src/app.po.ts
+++ nodebucket/e2e/src/app.po.ts
@@ -1,3 +1,12 @@
+/**
+ * Title: app.po.ts
+ * Author: Professor Krasso
+ * Date: 23 September 2020
+ * Modified By: Diandra McKenzie
+ * Description: App Promise file
+ */
+
+
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
