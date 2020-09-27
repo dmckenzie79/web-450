52b77c2cfe648d3047644164362bab120d7d14d5
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-09-27 10:45:04 -0400

    Update app.js

--- nodebucket/server/app.js
+++ nodebucket/server/app.js
@@ -1,4 +1,13 @@
 /**
+ * Title: app.js
+ * Author: Professor Krasso
+ * Date: 23 September 2020
+ * Modified By: Diandra McKenzie
+ * Description: API file
+ */
+
+
+/**
  * Require statements
  */
 const express = require('express');
@@ -7,6 +16,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
+const Employee = require('./models/employee'); //get the employees model from the Models directory
 
 /**
  * App configurations
@@ -19,12 +29,14 @@ app.use(express.static(path.join(__dirname, '../dist/nodebucket')));
 app.use('/', express.static(path.join(__dirname, '../dist/nodebucket')));
 
 /**
- * Variables
+
+
+* Variables
  */
 const port = 3000; // server port
 
 // TODO: This line will need to be replaced with your actual database connection string
-const conn = 'mongodb+srv://superadmin:s3cret@cluster0-lujih.mongodb.net/nodebucket?retryWrites=true&w=majority';
+const conn = 'mongodb+srv://nodebucket_user:S5FwoXuaVK4QDJpn@cluster-1.6d0ag.mongodb.net/nodebucket?retryWrites=true&w=majority';
 
 /**
  * Database connection
@@ -32,20 +44,59 @@ const conn = 'mongodb+srv://superadmin:s3cret@cluster0-lujih.mongodb.net/nodebuc
 mongoose.connect(conn, {
   promiseLibrary: require('bluebird'),
   useUnifiedTopology: true,
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useCreateIndex: true
 }).then(() => {
   console.debug(`Connection to the database instance was successful`);
 }).catch(err => {
-  console.log(`MongoDB Error: ${err.message}`)
+  console.log(`MongoDB Error: ${err.message}`);
 }); // end mongoose connection
 
-/**
- * API(s) go here...
+
+ /**
+ * FindEmployeeByID
+ *
  */
+app.get('/api/employees/:empId', async(req, res) => {
+  try {
+      /**
+       * Use the mongoose employee model to query MongoDB Atlas by employeeId
+       */
+      Employee.findOne({ 'empId': req.params.empId }, function(err, employee) {
+
+      /**
+       * If there is a database level error, handle by returning a server 500 error
+       */
+
+        if (err) {
+          console.log(err);
+          res.status(500).send({
+            message: 'Internal server error!'
+          });
+        } else {
+
+          /**
+           * If there is no database level errors, return the employee object
+           */
+          console.log(employee);
+          res.json(employee);
+        }
+      });
+
+  } catch(e) {
+      /**
+       * Catch any potential server errors and return a server 500 error
+       */
+      console.log(e);
+      res.status(500).send({
+          message: 'Internal server error!'
+      });
+  }
+});
 
 /**
  * Create and start server
  */
 http.createServer(app).listen(port, function() {
-  console.log(`Application started and listening on port: ${port}`)
+  console.log(`Application started and listening on port: ${port}`);
 }); // end http create server function
