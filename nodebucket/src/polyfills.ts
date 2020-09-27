e3838e71ff053a56f728f15818e2333c39a26311
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-09-27 10:50:09 -0400

    Update polyfills.ts

--- nodebucket/src/polyfills.ts
+++ nodebucket/src/polyfills.ts
@@ -1,4 +1,12 @@
 /**
+ * Title: polyfills.ts
+ * Author: Professor Krasso
+ * Date: 19 September 2020
+ * Modified By: Diandra McKenzie
+ * Description: Polyfills file
+ */
+
+/**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
  *
