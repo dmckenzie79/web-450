db584580da0bfc9c4c39126f24dad946cd4145b6
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-09-27 10:46:03 -0400

    Update app.module.ts

--- nodebucket/src/app/app.module.ts
+++ nodebucket/src/app/app.module.ts
@@ -1,3 +1,11 @@
+/**
+ * Title: app.module.ts
+ * Author: Professor Krasso
+ * Date: 19 September 2020
+ * Modified By: Diandra McKenzie
+ * Description: Module App file
+ */
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -11,16 +19,26 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AboutComponent } from './pages/about/about.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthGuard } from './shared/guard/auth.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     BaseLayoutComponent,
-    AuthLayoutComponent
+    AuthLayoutComponent,
+    AboutComponent,
+    SignInComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -32,8 +50,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
