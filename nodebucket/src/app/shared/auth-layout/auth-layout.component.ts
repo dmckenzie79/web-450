96f65d3563e859e5a052380e318984113f332678
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-09-27 10:48:39 -0400

    Update auth-layout.component.ts

--- nodebucket/src/app/shared/auth-layout/auth-layout.component.ts
+++ nodebucket/src/app/shared/auth-layout/auth-layout.component.ts
@@ -1,3 +1,11 @@
+/**
+ * Title: auth-layout.component.ts
+ * Author: Professor Krasso
+ * Date: 19 September 2020
+ * Modified By: Diandra McKenzie
+ * Description: Auth-Layout Component file
+ */
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
